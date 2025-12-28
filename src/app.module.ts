9b39912c296079aa7cdab53f8f237e54434591be
JimmyCo <dimamikron41@gmail.com>
2025-12-28 23:35:43 +0200

    feat: added rate limiter for 50 requests per minute

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SvitloModule } from './svitlo/svitlo.module';
 
 @Module({
-  imports: [SvitloModule],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000, // 60 секунд (1 минута)
+        limit: 50, // 50 запросов
+      },
+    ]),
+    SvitloModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
