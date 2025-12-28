366eda8aacb3af44d4a4da8f00c544c8e3415756
JimmyCo <dimamikron41@gmail.com>
2025-12-28 23:19:38 +0200

    upd: cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://svitlo-client.vercel.app/',
+      'https://svitlo-client-git-master-dmitrymass-projects.vercel.app/',
+      'https://svitlo-client-7be2rho5u-dmitrymass-projects.vercel.app/',
+    ],
     credentials: true,
   });
 
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
+  console.log(
+    `Application is running on: http://localhost:${process.env.PORT ?? 3000}`,
+  );
 }
 bootstrap();
